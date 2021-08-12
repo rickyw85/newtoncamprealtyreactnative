06dbddb58b593e3070c20d3f51ea104bc6c690aa
rickyw85 <83381367+rickyw85@users.noreply.github.com>
2021-08-12 19:59:01 -0400

    Delete vendors.js

--- vendors.js
+++
@@ -1,8 +0,0 @@
-export const VENDORS = [
-    {
-        id: 0,
-        image: "/assets/images/forsale.jpg",
-        featured: true,
-        description: "Contact us about becoming one of our local business vendors. We look forward to working with you in the future."
-    }
-]
