e832558395aa6cde23f89ff8e216dad582479448
rickyw85 <83381367+rickyw85@users.noreply.github.com>
2021-08-12 20:11:18 -0400

    Delete newton.js

--- newton.js
+++
@@ -1,9 +0,0 @@
-export const NEWTON = [
-    {
-        id: 0,
-        name: "Newton Camp",
-        image: "/assets/images/newton1.jpg",
-        featured: true,
-        description: "People know what they want five seconds after they see a place if they really want it. Get up, walk up to the listing, and think about what they see in those five seconds."
-    }
-]
